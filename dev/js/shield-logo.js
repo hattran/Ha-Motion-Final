e7313437c31cee2f7e31f6fb868e359d6753c716
hattran <63318539+hattran@users.noreply.github.com>
2020-11-17 18:29:22 -0500

    added gsap bonus

--- dev/js/shield-logo.js
+++ dev/js/shield-logo.js
@@ -1,13 +1,15 @@
 import {gsap} from "gsap";
+import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
+
+gsap.registerPlugin(DrawSVGPlugin);
 
 gsap.set("#shield-logo",{fill:"none"});
 
 const logoTL = gsap.timeline();
 
-
 export function logoAnimation(){
 
-    logoTL.to("#bird",{drawSVG:"100%"});
+    logoTL.from("#bird",{duration:2, drawSVG:"100%"});
 
     return logoTL;
 }
