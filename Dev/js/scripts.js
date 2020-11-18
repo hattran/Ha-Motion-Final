0cd5380095d1cb21894bd94c67ccf2cb65ea86bc
hattran <63318539+hattran@users.noreply.github.com>
2020-11-17 22:20:20 -0500

    animated shield logo

--- Dev/js/scripts.js
+++ Dev/js/scripts.js
@@ -1,10 +1,12 @@
 import {gsap} from "gsap";
-// import {GSDevTools} from "gsap/GSDevTools";
+import {GSDevTools} from "gsap/GSDevTools";
 
 import {logoAnimation} from "./shield-logo.js"
+
+gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline();
 mainTL.add(logoAnimation());
 
-//instantiate GSDevTools with default settings
-// GSDevTools.create();
+// instantiate GSDevTools with default settings
+GSDevTools.create();
--- dev/js/shield-logo.js
+++ dev/js/shield-logo.js
@@ -3,13 +3,18 @@ import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-gsap.set("#shield-logo",{fill:"none"});
+gsap.set("#shield-logo",{transformOrigin:"center",fill:"none"});
 
 const logoTL = gsap.timeline();
 
 export function logoAnimation(){
 
-    logoTL.from("#bird",{duration:2, drawSVG:"100%"});
+    logoTL.from("#bird",{duration:3, drawSVG:"50% 50%"},"logo-start")
+        .from("#tail",{duration:3, drawSVG:"60% 60%"},"logo-start")
+        .from(".stripes",{duration:3, drawSVG:"0%"},"logo-start")
+        .from("#border",{duration:3, drawSVG:"50% 50%"},"logo-start")
+        .to("#shield-logo",{duration:2,fill:"#FFF", delay:"-1"})
+        .to("#shield-logo",{duration:1,scale:"0.3", x:-100, y:-150});
 
     return logoTL;
 }
