2d0bec0ac4b845c89bc48bccf06b8652eb08b60c
hattran <63318539+hattran@users.noreply.github.com>
2020-11-23 01:57:19 -0500

    spiced up logo animation

--- dev/js/shield-logo.js
+++ dev/js/shield-logo.js
@@ -13,7 +13,10 @@ export function logoAnimation(){
         .from("#tail",{duration:3, drawSVG:"60% 60%"},"logo-start")
         .from(".stripes",{duration:3, drawSVG:"0%"},"logo-start")
         .from("#border",{duration:3, drawSVG:"50% 50%"},"logo-start")
-        .to("#shield-logo",{duration:2,fill:"#FFF", delay:"-1"})
+        .to("#border",{duration:2,fill:"#FFF", delay:"-1"})
+        .to("#tail",{duration:1,fill:"#FFF", delay:"-1"})
+        .to("#bird",{duration:1,fill:"#FFF", delay:"-0.5"})
+        .to(".stripes",{duration:1,fill:"#FFF", delay:"-0.5", stagger:".25"})
         .to("#shield-logo",{duration:1,scale:"0.3", x:-100, y:-150});
 
     return logoTL;
