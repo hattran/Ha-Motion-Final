9efc817281bc2057efb6b09329befd9bad82a622
hattran <63318539+hattran@users.noreply.github.com>
2020-12-07 20:47:59 -0500

    prepped for presentation

--- dev/js/content/main-screen.js
+++ dev/js/content/main-screen.js
@@ -5,7 +5,9 @@ gsap.registerPlugin(DrawSVGPlugin);
 
 gsap.set("#return",{alpha:0});
 gsap.set("#errors",{alpha:0});
-gsap.set("#zephyr-icon-map",{transformOrigin:"center"})
+gsap.set("#round-button-blue",{alpha:0,transformOrigin:"center"});
+gsap.set(".targets",{transformOrigin:"center"});
+gsap.set("#zephyr-icon-map",{transformOrigin:"center"});
 
 const mainscreenTL = gsap.timeline();
 
@@ -14,27 +16,27 @@ export function mainscreenAnimation(){
             .from("#top-line",{duration:1,drawSVG:0},"flavor")
             .from("#zephyr-corner-stroke",{duration:2, drawSVG:"50% 50%"})
             .from("#zephyr-corner-stroke",{duration:1,scale:4, x:300, y:-400})
-            .from(".panel",{duration:2,drawSVG:"0"})
+            .from(".panel",{duration:2,drawSVG:"0",stagger:.1})
             .from(".panel-text",{duration:1,scaleX:0}, "zephyr-appear")
             .from("#zephyr-corner-fill",{duration:2,alpha:0}, "zephyr-appear")
             .from("#inhuman-profile",{duration:2,drawSVG:0},"zephyr-appear")
             .from("#target-info-text",{duration:2,scaleX:0}, "zephyr-appear")
             .from(".zephyr-lines",{duration:2,drawSVG:0,stagger:0.25},"zephyr-appear")
             .from(".zephyr-box",{duration:2,drawSVG:0,stagger:0.25},"zephyr-appear")
-            .from(".text-type-in",{duration:2,scaleX:0,stagger:0.25}, "zephyr-appear")
+            .from(".text-type-in",{duration:1,scaleX:0,stagger:0.2}, "zephyr-appear")
             .from(".decorations",{duration:2,alpha:0}, "zephyr-appear")
+            .from("#bottom-line",{duration:1,drawSVG:0},"zephyr-appear")
+            .from("#round-button",{duration:1,drawSVG:"0"},"zephyr-appear")
+            .from("#dashboard",{duration:.5,scaleX:0},"zephyr-appear")
             .from("#world-map",{duration:2,alpha:0,delay:"-1.5"})
             .from("#zephyr-icon-map",{scale:0,duration:1,delay:"-1"})
             .fromTo(".targets",{scale:0},{scale:1,duration:1,stagger:0.2,delay:"-2"})
             .fromTo(".targets",{alpha:1},{alpha:0,duration:1,stagger:0.2,repeat:5})
             .from("#play-button-fill",{duration:1,alpha:0,delay:"-12"},"music")
             .from("#play-button-stroke",{duration:1,drawSVG:0,delay:"-12"})
-            .from("#song-title",{duration:1,scaleX:0,delay:"-11"})
-            .from("#song-artist",{duration:1,scaleX:0,delay:"-10"})
-            .from("#bottom-line",{duration:1,drawSVG:0,delay:"-9"})
-            .from("#round-button",{duration:1,drawSVG:"0",delay:"-8"})
-            .from("#round-button-blue",{duration:1,drawSVG:"0",delay:"-7"})
-            .from("#dashboard",{duration:1,scaleX:0,delay:"-7"});
+            .from("#song-title",{duration:.5,scaleX:0,delay:"-11"})
+            .from("#song-artist",{duration:.5,scaleX:0,delay:"-10.5"});
+            // .fromTo("#round-button-blue",{drawsvg:"0"},{alpha:0,duration:1,drawsvg:"20 50",repeat:3})
 
     return mainscreenTL;
 }
