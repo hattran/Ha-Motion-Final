0a0bc88415bfa712ec7be445472a3122cc757e67
hattran <63318539+hattran@users.noreply.github.com>
2020-12-10 00:42:59 -0500

    removed dev tools?

--- Dev/js/scripts.js
+++ Dev/js/scripts.js
@@ -1,12 +1,12 @@
 import {gsap} from "gsap";
-import {GSDevTools} from "gsap/GSDevTools";
+// import {GSDevTools} from "gsap/GSDevTools";
 import {logoAnimation} from "./shield-logo.js";
 import {mainscreenAnimation} from "./content/main-screen.js";
 import {dashboardAnimation} from "./content/dashboard.js";
 import {malfunction} from "./content/dashboard.js";
 import {logoTwist} from "./shield-logo.js";
 
-gsap.registerPlugin(GSDevTools);
+// gsap.registerPlugin(GSDevTools);
 
 var audio = document.getElementById("audio");
 
@@ -24,4 +24,4 @@ function playMusic(){
 }
 
 // instantiate GSDevTools with default settings
-GSDevTools.create();
+// GSDevTools.create();
