686293c4dc0e44ffab74f90ba3122e406277af8a
hattran <63318539+hattran@users.noreply.github.com>
2020-12-10 00:25:40 -0500

    added music

--- Dev/js/scripts.js
+++ Dev/js/scripts.js
@@ -6,8 +6,11 @@ import {dashboardAnimation} from "./content/dashboard.js";
 import {malfunction} from "./content/dashboard.js";
 import {logoTwist} from "./shield-logo.js";
 
-
 gsap.registerPlugin(GSDevTools);
+
+var audio = document.getElementById("audio");
+
+playMusic();
 
 const mainTL = gsap.timeline();
 mainTL.add(logoAnimation())
@@ -15,6 +18,10 @@ mainTL.add(logoAnimation())
 .add(dashboardAnimation())
 .add(malfunction())
 .add(logoTwist());
+
+function playMusic(){
+    audio.play();
+}
 
 // instantiate GSDevTools with default settings
 GSDevTools.create();
