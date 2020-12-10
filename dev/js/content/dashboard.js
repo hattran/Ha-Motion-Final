08efbf26c9fe171348bb6127cc107391ef3bd754
hattran <63318539+hattran@users.noreply.github.com>
2020-12-09 19:05:05 -0500

    animate all dashboard elements (appear)

--- dev/js/content/dashboard.js
+++ dev/js/content/dashboard.js
@@ -2,6 +2,7 @@ import {gsap} from "gsap";
 import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
+gsap.set("#air-progress-dial",{transformOrigin:"center",rotation:"270"});
 
 const dashboardTL = gsap.timeline();
 
@@ -19,7 +20,12 @@ export function dashboardAnimation(){
                 .from(".centers",{duration:0.5,scale:0,transformOrigin:"center"},"info-appear")
                 .from(".numbers",{duration:0.5,alpha:0},"info-appear")
                 .from(".dashboard-text",{duration:0.5,scaleY:0},"info-appear")
-                .from(".handles",{duration:0.5,alpha:0},"info-appear");
+                .from(".fade",{duration:0.5,alpha:0},"info-appear")
+                .from(".draw",{duration:0.5,drawSVG:0},"info-appear")
+                .from("#compass-inner-left",{duration:0.5,scaleY:0, transformOrigin:"bottom"},"info-appear")
+                .from("#compass-inner-right",{duration:0.5,scaleY:0, transformOrigin:"top"},"info-appear")
+                .from("#compass-inner-center",{duration:0.5,scaleY:0},"info-appear")
+                .fromTo("#air-progress-dial",{drawSVG:"0% 0%"},{duration:0.5,drawSVG:"0% 75%"});
 
     return dashboardTL;
 }
--- dev/js/content/main-screen.js
+++ dev/js/content/main-screen.js
@@ -7,26 +7,28 @@ gsap.set("#round-button",{transformOrigin:"center",rotation:"270"});
 gsap.set(".targets",{transformOrigin:"center"});
 gsap.set(".glows",{transformOrigin:"center"});
 gsap.set("#zephyr-icon-map",{transformOrigin:"center"});
+gsap.set(".dashboard-text",{transformOrigin:"center"});
+gsap.set(".panel-text",{transformOrigin:"center"});
 
 const mainscreenTL = gsap.timeline();
 
 export function mainscreenAnimation(){
-    mainscreenTL.from(".flavor-text-appear",{duration:1,scaleX:0},"flavor")
+    mainscreenTL.from(".flavor-text-appear",{duration:0.5,scaleX:0},"flavor")
             .from("#top-line",{duration:1,drawSVG:0},"flavor")
             .from("#zephyr-corner-stroke",{duration:2, drawSVG:"50% 50%"})
             .from("#zephyr-corner-stroke",{duration:1,scale:4, x:300, y:-400})
             .from(".panel",{duration:2,drawSVG:"0",stagger:.1})
-            .from(".panel-text",{duration:1,scaleX:0}, "zephyr-appear")
+            .from(".panel-text",{duration:1,scaleY:0}, "zephyr-appear")
             .from("#zephyr-corner-fill",{duration:2,alpha:0}, "zephyr-appear")
             .from("#inhuman-profile",{duration:2,drawSVG:0},"zephyr-appear")
             .from("#target-info-text",{duration:2,scaleX:0}, "zephyr-appear")
             .from(".zephyr-lines",{duration:2,drawSVG:0,stagger:0.25},"zephyr-appear")
             .from(".zephyr-box",{duration:2,drawSVG:0,stagger:0.25},"zephyr-appear")
-            .from(".text-type-in",{duration:1,scaleX:0,stagger:0.2}, "zephyr-appear")
+            .from(".text-type-in",{duration:1,scaleY:0,stagger:0.2}, "zephyr-appear")
             .from(".decorations",{duration:2,alpha:0}, "zephyr-appear")
             .from("#bottom-line",{duration:1,drawSVG:0},"zephyr-appear")
             .from("#round-button",{duration:1,drawSVG:"0"},"zephyr-appear")
-            .from("#dashboard",{duration:.5,scaleX:0},"zephyr-appear")
+            .from("#dashboard",{duration:.5,scaleY:0},"zephyr-appear")
             .from("#world-map",{duration:2,alpha:0,delay:"-1.5"})
             .from("#zephyr-icon-map",{scale:0,duration:1,delay:"-1"})
             .fromTo(".targets",{scale:0},{scale:1,duration:1,stagger:0.2,delay:"-2"})
